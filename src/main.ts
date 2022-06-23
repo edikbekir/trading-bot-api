6a89d9e5fe5300a2519824c784f4e797a0b685e5
Eduard Bekir <edikbekir@rambler.ru>
2022-06-23 15:47:56 +0200

    CORS

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   );
 
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://bitrade.tech');
+    res.header('Access-Control-Allow-Origin', 'https://www.bitrade.tech');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
     next();
